c00b67390327f7d4af3c32faed3a7f58d6ec1c9f
belo <69517797+beloele@users.noreply.github.com>
2024-08-25 17:21:28 +0300

    aaa

---
+++ view/login.php
@@ -0,0 +1,54 @@
+<main>
+   <div class="observer" style="top: 70px;"></div>
+   <section data-v-c70e1c26="" class="login login--d">
+      <div data-v-c70e1c26="" class="login__container">
+         <div data-v-c70e1c26="" class="login__promotions">
+            <h2 data-v-c70e1c26="">PROMOSYONLARIMIZ</h2>
+            <ul data-v-c70e1c26="">
+               <li data-v-c70e1c26=""><button data-v-c70e1c26=""><img data-v-c70e1c26="" src="https://d3ejb2l5e3bvmc.cloudfront.net/production/promotions/promotion_137_37_v6.gif" alt="HERKES 3, ŞANSI HAKEDER"></button></li>
+               <li data-v-c70e1c26=""><button data-v-c70e1c26=""><img data-v-c70e1c26="" src="https://d3ejb2l5e3bvmc.cloudfront.net/production/promotions/promotion_176_37_v3.gif" alt="%100 HAFTA SONU BONUSU"></button></li>
+               <li data-v-c70e1c26=""><button data-v-c70e1c26=""><img data-v-c70e1c26="" src="https://d3ejb2l5e3bvmc.cloudfront.net/production/promotions/promotion_135_37_v3.gif" alt="%50 PAZARTESİ BONUSU"></button></li>
+               <li data-v-c70e1c26=""><button data-v-c70e1c26=""><img data-v-c70e1c26="" src="https://d3ejb2l5e3bvmc.cloudfront.net/production/promotions/promotion_142_37_v3.gif" alt="%40 PERŞEMBE BONUSU"></button></li>
+            </ul>
+         </div>
+         <form data-v-c70e1c26="">
+            <div data-v-c70e1c26="" class="login__brand">
+               <img data-v-c70e1c26="" src="/images/redesign/logo-dark.png" alt="MeritKing"> 
+               <h3 data-v-c70e1c26="">UMUT DEĞİL PARA DAĞITIYORUZ!</h3>
+            </div>
+            <div data-v-c70e1c26="" class="login__form">
+               <div data-v-192295e2="" data-v-c70e1c26="" class="auth-input auth-input--light">
+                  <div data-v-192295e2="" class="auth-input__wrapper">
+                     <div data-v-192295e2="" class="auth-input__root">
+                        <div data-v-192295e2="" class="auth-input__icon"><i data-v-3ee02676="" data-v-192295e2="" class="icon-user icon--md"></i></div>
+                        <input data-v-192295e2="" type="text" placeholder="Kullanıcı Adınız" class="input--reset"> <!---->
+                     </div>
+                  </div>
+                  <!---->
+               </div>
+               <div data-v-192295e2="" data-v-c70e1c26="" class="auth-input auth-input--light">
+                  <div data-v-192295e2="" class="auth-input__wrapper">
+                     <div data-v-192295e2="" class="auth-input__root">
+                        <div data-v-192295e2="" class="auth-input__icon"><i data-v-3ee02676="" data-v-192295e2="" class="icon-lock icon--md"></i></div>
+                        <input data-v-192295e2="" type="password" placeholder="Şifreniz" class="input--reset"> <button data-v-192295e2="" type="button" class="auth-input__toggle"><i data-v-3ee02676="" data-v-192295e2="" class="icon-eye-slash icon--sm"></i></button>
+                     </div>
+                  </div>
+                  <!---->
+               </div>
+               <button data-v-c70e1c26="" type="submit" class="btn btn--block btn--uppercase">
+               Giriş Yap
+               </button> <a data-v-c70e1c26="" href="/auth/forgot-password" class="login__forgot"><i data-v-3ee02676="" data-v-c70e1c26="" class="icon-bell icon--lg"></i> <span data-v-c70e1c26="">Şifrenizi mi unuttunuz?</span></a> 
+               <div data-v-c70e1c26="" class="turnstile">
+                  <div class="turnstile__wrapper">
+                     <div data-theme="light">
+                        <div style="border: 0px; margin: 0px; padding: 0px;"><input type="hidden" name="cf-turnstile-response" id="cf-chl-widget-vtynu_response" value="0.DuYRSb_oTT5sC22t6SCcCm7xNkV4deSUIGXFFTO9bayekiYnWr90_x-Xh7nB2nD979JJinkzJEYv9mjWXphLHJRqPVWTkQQorfIsidM_cNogertdlcneAHQ_U3CGvQm83ktTiuKMknSJLgLQiy23AwOaItl0om7Lama8u-cgJIA6f999QzK_-koQ6HzgG_df9RpCt3rVm1k6JeMJcTjO9JmB7RjeWR4AgTQLWZS7hs5N11IlaHBDSf-kvanbk1JvR9f8O-CsFEOEnXXMEzieDmEEAANCYXlrD0Yf1hD1xIN0X89V6Ptqne1W5fIFO26rjX2kO6ygeCrdOduwTvT5JVE_2I92SHBxLbK8iZMaS8bQCy_tF5jcGWvKMCnn-b_CCtmFP-nmSn1vPwaUnK-iIDi90mnErAi6ixdVMQLqpGaBZ2tFG1xiQsLklLM-QlSF.qAMzuxOdwM6S57kU8g-L_g.49217ce85723cff3f335c0b7a06b84a67d399f9e19c308ff34de70e1eb396f6f"></div>
+                     </div>
+                  </div>
+                  <!---->
+               </div>
+               <p data-v-c70e1c26="">Cloudflare is a global cloud platform designed to make everything you connect to the Internet secure, private, fast and reliable.</p>
+            </div>
+         </form>
+      </div>
+   </section>
+</main>

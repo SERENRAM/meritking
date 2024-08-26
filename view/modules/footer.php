8ccad53b166b41f96f1e9040716592f33aa0f6c3
belo <69517797+beloele@users.noreply.github.com>
2024-08-26 18:01:13 +0300

    aaa

--- view/modules/footer.php
+++ view/modules/footer.php
@@ -139,7 +139,7 @@
             </div>
          </footer>
          <div class=app-drawer__root>
-            <div class="app-drawer__backdrop"></div>
+            <div class="app-drawer__backdrop hidden"></div>
             <div class="app-drawer app-drawer--default app-drawer--left app-drawer--primary">
                <div data-v-61f084f0 class=side-menu>
                   <div data-v-61f084f0 class=side-menu__teaser>
